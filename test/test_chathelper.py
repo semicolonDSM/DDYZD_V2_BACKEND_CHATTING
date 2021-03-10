1ef7ae4fad734f519728bfca575fc7bc0c9d9da6
hwc9169 <hwc9169@gmail.com>
2021-03-10 17:22:53 +0900

    UPDATE; test helper_cancel_applicant

--- test/test_chathelper.py
+++ test/test_chathelper.py
@@ -12,10 +12,8 @@ def test_helper_apply(db_setting, flask_websocket, flask_client, flask_app):
     flask_websocket.emit('helper_apply', {'room_token': room_token(user_id=4, room_id=3, user_type='U'), 'major': '프론트엔드'}, namespace='/chat')
     recv = flask_websocket.get_received(namespace='/chat')[1]
 
-    assert recv['name'] == 'recv_chat'
-    assert recv['args'][0]['date'] != None
-    assert recv['args'][0]['title'] == '성예인님이 동아리에 지원하셨습니다'
-    assert recv['args'][0]['msg'] == '1110 성예인님이 세미콜론에 프론트엔드 분야로 지원하셨습니다'
+    assert recv['name'] == 'alarm'
+    assert recv['args'][0]['room_id'] != 3
 
     flask_websocket.emit('helper_apply', {'room_token': room_token(user_id=3, room_id=2, user_type='U'), 'major': '프론트엔드'}, namespace='/chat')
     recv = flask_websocket.get_received(namespace='/chat')[0]
@@ -36,7 +34,6 @@ def test_helper_schedule(db_setting, flask_websocket, flask_client):
 
 
 def test_helper_result(db_setting, flask_websocket, flask_client):
-    logger.info(Room.query.get(2).status)
     flask_websocket.emit('join_room', {'room_token': room_token(room_id=2)}, namespace='/chat')
     flask_websocket.emit('helper_result', {'result': True, 'room_token': room_token(room_id=2)}, namespace='/chat')
     recv = flask_websocket.get_received(namespace='/chat')[1]
@@ -65,8 +62,9 @@ def test_join_room(flask_websocket, db_setting):
 ## 채팅 보내기 테스트 ## 
 def test_send_chat(db_setting, flask_websocket):
     # 동아리장 채팅
+    flask_websocket.emit('join_room', {'room_token': room_token()}, namespace='/chat')
     flask_websocket.emit('send_chat',{'msg': 'Hello \U0001f600', 'room_token': room_token()}, namespace='/chat')
-    recv = flask_websocket.get_received(namespace='/chat')[0]
+    recv = flask_websocket.get_received(namespace='/chat')[1]
 
     assert recv['name'] == 'recv_chat'
     assert recv['args'][0]['msg'] == 'Hello \U0001f600'
@@ -77,9 +75,8 @@ def test_send_chat(db_setting, flask_websocket):
     flask_websocket.emit('send_chat', {'msg': 'World!', 'room_token': room_token(user_id=2, user_type='U')}, namespace='/chat')
     recv = flask_websocket.get_received(namespace='/chat')[0]
 
-    assert recv['name'] == 'recv_chat'
-    assert recv['args'][0]['title'] == None
-    assert recv['args'][0]['msg'] == 'World!'
+    assert recv['name'] == 'alarm'
+    assert recv['args'][0]['room_id'] == '1'
 
 
 ## 방 나가기 테스트 (채팅 보내고 난 뒤에 실행 되어야함!) ## 
@@ -88,3 +85,19 @@ def test_leave_room(flask_websocket, db_setting):
     recv = flask_websocket.get_received(namespace='/chat')[0]
 
     assert recv['args'][0]['msg'] == 'Leave Room Success'    
+
+
+def test_helper_cancel_applicant(db_setting, flask_websocket, flask_client):
+    flask_websocket.emit('helper_cancel_applicant', {'room_token': room_token(room_id=3)}, namespace='/chat')
+    recv = flask_websocket.get_received(namespace='/chat')[0]
+
+    assert recv['name'] == 'recv_chat'
+    assert recv['args'][0]['date'] != None
+    assert recv['args'][0]['title'] == '세미콜론 동아리의 지원이 취소 되었습니다'
+    assert recv['args'][0]['msg'] == '성예인님의 동아리 지원이 취소 되었습니다'
+
+    flask_websocket.emit('helper_cancel_applicant', {'room_token': room_token(room_id=3, user_type='C')}, namespace='/chat')
+    recv = flask_websocket.get_received(namespace='/chat')[0]
+
+    assert recv['name'] == 'error'
+    assert recv['args'][0]['msg'] == '400: User is not applicant or scheduled'
