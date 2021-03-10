3cca4a297e963ffc2ae2d7502f57c35d01bf4e6d
hwc9169 <hwc9169@gmail.com>
2021-03-10 11:58:57 +0900

    UPDATE; cancel_applicant_required

--- app/decorator.py
+++ app/decorator.py
@@ -94,10 +94,9 @@ def send_alarm(fn):
         if not recv_user.is_in_room(room):
             asyncio.run(fcm_alarm(sender=send_user, msg=msg, token=recv_user.device_token, 
                 room_id=room.id, user_type=user_type))
-            # title: 보내는 사람 이름 혹은 보내는 동아리 이름
-            # msg: 일반 유저인 경우 일반 메시지, 봇인 경우 제목을 전송
-            emit('alarm', {'room_id': str(json.get('room_id'))}, room=recv_user.session_id)
-
+        # title: 보내는 사람 이름 혹은 보내는 동아리 이름
+        # msg: 일반 유저인 경우 일반 메시지, 봇인 경우 제목을 전송
+        emit('alarm', {'room_id': str(json.get('room_id'))}, room=recv_user.session_id)
 
         return fn(json)
     return wrapper
@@ -287,6 +286,36 @@ def answer_required(fn):
             return emit('error', error.BadRequest('The user is not resulted'), namespace='/chat') 
 
         json['title'], json['msg'] = get_answer_message(user, club, json.get('answer'))
+        json['fcm_type'] = FcmType.H.name # fcm 알림을 보낼 때 사용할 봇이 보낸 메시지임을 알려둠
+
+        return fn(json)
+    return wrapper
+
+
+def get_cancel_applicant_message(user, club):
+    title = "{club} 동아리의 지원이 취소 되었습니다".format(name=club.name)
+    msg = "{name}님의 동아리 지원이 취소 되었습니다".format(name=user.name)
+
+    return title, msg
+
+
+def cancel_applicant_required(fn):
+    '''
+    요약: 지원자 삭제하는 데코레이터
+    삭제시 메시지와 권한 필터링
+    '''
+    @wraps(fn)
+    def wrapper(json):
+        user = json.get('room').user
+        club = json.get('club')
+        # 동아리 장이 아닌 경우
+        if json.get('user_type') == 'C':
+            return emit('error', error.Forbidden('You are not a head for the club'), namespace='/chat')
+        # 동아리의 지원자가 아닌 경우
+        if not (user.is_applicant(club) or user.is_scheduled(club)):
+            return emit('error', error.BadRequest('User is not applicant or scheduled'), namespace='/chat')
+
+        json['title'], json['msg'] = get_cancel_applicant_message(user, club)
         json['fcm_type'] = FcmType.H.name # fcm 알림을 보낼 때 사용할 봇이 보낸 메시지임을 알려둠
 
         return fn(json)
