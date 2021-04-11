8746bdf3a7e1c868f3ba49ca8634773552fea531
hwc9169 <hwc9169@gmail.com>
2021-04-11 15:22:46 +0900

    FIX; no log file in test code

--- test/test_helper_schedule.py
+++ test/test_helper_schedule.py
@@ -5,8 +5,6 @@ def test_helper_schedule(db_setting, flask_websocket, flask_client):
     flask_websocket.emit('join_room', {'room_token': room_token(room_id=2)}, namespace='/chat')
     flask_websocket.emit('helper_schedule', {'date': '2020년 6월 18일 오후 6시 10분', 'location': '3층 그린존','room_token': room_token(room_id=2)}, namespace='/chat')
     recv = flask_websocket.get_received(namespace='/chat')[2]
-    from app import logger
-    logger.info(recv)
 
     assert recv['name'] == 'recv_chat'
     assert recv['args'][0]['date'] != None
