7c8d11860c2c6127256b8bda01ef6ddd70a67c2e
hwc9169 <hwc9169@gmail.com>
2021-04-14 22:54:47 +0900

    REFACTOR; change event to api

--- app/api_v1_0/event_api/delete_room.py
+++
@@ -1,9 +0,0 @@
-from app.decorator.room_token_required import room_token_required
-from app.decorator.room_read import room_read
-from flask_socketio import join_room
-from flask_socketio import emit
-
-@room_token_required
-def event_delete_room(json):
-    room = json.get('room')
-    room.delete_chats(json.get('user_type'))
